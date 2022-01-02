8b659d987d59f9265893f7054a9af9ad2f254856
Marcio Cruz de Almeida <marciocadev@gmail.com>
2022-01-02 09:57:25 -0300

    feat(update): update project

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,12 +1,35 @@
-import { typescript } from "projen";
+import { typescript } from 'projen';
 const project = new typescript.TypeScriptProject({
-  defaultReleaseBranch: "main",
-  name: "aws-laziness",
+  defaultReleaseBranch: 'main',
+  name: 'aws-laziness',
   projenrcTs: true,
 
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
+  repository: 'https://github.com/marciocadev/mca-lazyless.git',
+  release: true,
+  releaseToNpm: true,
+
+  bin: {
+    'aws-laziness': 'lib/index.js',
+  },
+  deps: ['projen'],
+  devDeps: [
+    '@types/node-notifier',
+    '@types/cli-color',
+  ],
+  bundledDeps: [
+    'cli-color',
+    'node-notifier',
+    'commander',
+  ],
+
+  codeCov: true,
+  docgen: true,
+  eslint: true,
+  tsconfig: {
+    compilerOptions: {
+      lib: ['dom', 'es2019'],
+      resolveJsonModule: true,
+    },
+  },
 });
 project.synth();
