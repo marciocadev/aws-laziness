119f4bef298b7446bb8c8ff980f3c5405822466a
Marcio Cruz de Almeida <marciocadev@gmail.com>
2022-01-03 15:06:32 -0300

    feat: correct version

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
+import * as fs from 'fs';
+import * as path from 'path';
 import { program } from 'commander';
-//import { version } from './version-aws-laziness.json';
+const json = JSON.parse(fs.readFileSync(path.resolve('../package.json'), 'utf8'));
+const version = json.version;
 import './commands/dynamodb-schema';
-//program.version(version, '-v, --version', 'output current version');
+program.version(version, '-v, --version', 'output current version');
 program.parse(process.argv);
-
