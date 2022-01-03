c42ee3af7673a6384731ae57991e59226c18b8a6
Marcio Cruz de Almeida <marciocadev@gmail.com>
2022-01-03 12:17:36 -0300

    fix: remove Laziness class

--- test/lazy/aws-sdk/v3/aws-dynamodb/lazy-schema.test.ts
+++ test/lazy/aws-sdk/v3/aws-dynamodb/lazy-schema.test.ts
@@ -1,3 +1,8 @@
-test('hello1', () => {
-  expect(true);
+import { LazyDynamoDBSchema2 } from '../../../../../src/lazy/aws-sdk/v3/aws-dynamodb/lazy-schema-2';
+
+test('teste', () => {
+  const lazy = new LazyDynamoDBSchema2('schema', 'Test', '/home/marcio/marciocadev/aws-laziness/test/new');
+  lazy.createModel({
+    partitionKey: { key: 'company', type: 'string' },
+  });
 });
