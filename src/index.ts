7503dc9e80ee7ff62deafe36b0ab721a114fc8e3
Marcio Cruz de Almeida <marciocadev@gmail.com>
2022-01-03 15:43:10 -0300

    feat: fix workflow

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 //import * as fs from 'fs';
 //import * as path from 'path';
 import { program } from 'commander';
+import { version } from './package.json';
 import './commands/dynamodb-schema';
-//program.version(version, '-v, --version', 'output current version');
+program.version(version, '-v, --version', 'output current version');
 program.parse(process.argv);
