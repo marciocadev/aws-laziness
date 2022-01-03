dba4fe545d1418a0abdc968777984cc0be578344
Marcio Cruz de Almeida <marciocadev@gmail.com>
2022-01-03 15:19:50 -0300

    fix: remove version

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
 
-import * as fs from 'fs';
-import * as path from 'path';
+//import * as fs from 'fs';
+//import * as path from 'path';
 import { program } from 'commander';
-const json = JSON.parse(fs.readFileSync(path.resolve('../package.json'), 'utf8'));
-const version = json.version;
 import './commands/dynamodb-schema';
-program.version(version, '-v, --version', 'output current version');
+//program.version(version, '-v, --version', 'output current version');
 program.parse(process.argv);
